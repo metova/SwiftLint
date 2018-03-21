a5a53363ffc03932d9a0f73763aac666cebddd53
Łukasz Szpyrka <lukasz.szpyrka@metova.com>
2018-03-21 15:46:25 +0100

    creating api connection

---
+++ Source/SwiftLintFramework/OpenApi.swift
@@ -0,0 +1,21 @@
+//
+//  OpenApi.swift
+//  swiftlint
+//
+//  Created by Łukasz Szpyrka on 21/03/2018.
+//  Copyright © 2018 Realm. All rights reserved.
+//
+
+import Foundation
+
+public struct OpenApi {
+
+    public init () {
+
+    }
+
+    public func run() {
+
+        print("will run lint command")
+    }
+}
